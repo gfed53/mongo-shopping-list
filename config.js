772c2a92af4ef5810c42753e11478a2c8426ecd0
gfed53 <gfederico7153@gmail.com>
2017-05-27 12:38:04 -0400

    Updated config to point to mLab db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://gFedz4321:33bask873F@ds155841.mlab.com:55841/heavenly-whirlwind' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
